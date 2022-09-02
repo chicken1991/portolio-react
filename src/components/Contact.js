1dce665f5633624646b41da7f2c9d1cff7062b95
chicken1991 <77861966+chicken1991@users.noreply.github.com>
2022-09-01 23:01:03 -0700

    Some changes to form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import img1 from '../assets/img/team/me.jpg';
 
+//form helpers
+import { validateEmail } from '../utils/helpers';
+
+
 export default function Contact() {
 
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+    const [message, setMessage] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const handleInputChange = (e) => {
+        // Getting the value and name of the input which triggered the change
+        const { target } = e;
+        const inputType = target.name;
+        const inputValue = target.value;
+
+        // Based on the input type, we set the state of either email, username, and password
+        if (inputType === 'email') {
+            setEmail(inputValue);
+        } else if (inputType === 'Name') {
+            setName(inputValue);
+        } else if (inputType === 'Message') {
+            setMessage(inputValue);
+        }
+    };
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+
+        if (!validateEmail(email)) {
+            setErrorMessage('Please enter a valid email address');
+            return;
+        }
+        if (!name) {
+            setErrorMessage(
+                `Please enter your name`
+            );
+            return;
+        }
+        if (!message) {
+            setErrorMessage(
+                `Please enter a message`
+            );
+            return;
+        }
+
+        // If everything goes according to plan, we want to clear out the input after a successful registration.
+        setName('');
+        setMessage('');
+        setEmail('');
+    };
 
     return (
         <div>
@@ -36,21 +86,32 @@ export default function Contact() {
                             </div>
                         </div>
                         <Form className="col-sm-4 mx-auto">
-                            <Form.Group className="mb-3" controlId="formEmail">
+                        <Form.Group className="mb-3" controlId="formName" onChange={handleInputChange}>
+                                <Form.Label>Name</Form.Label>
+                                <Form.Control type="name" placeholder="Enter your name" />
+                                <Form.Text className="text-muted">
+                                </Form.Text>
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="formEmail" onChange={handleInputChange}>
                                 <Form.Label>Email address</Form.Label>
                                 <Form.Control type="email" placeholder="Enter email" />
                                 <Form.Text className="text-muted">
                                 </Form.Text>
                             </Form.Group>
 
-                            <Form.Group className="mb-3" controlId="formControlsTextarea">
+                            <Form.Group className="mb-3" controlId="formControlsTextarea" onChange={handleInputChange}>
                                 <Form.Label>Message:</Form.Label>
-                                <Form.Control as="textarea" rows={3} type="message" placeholder="Enter your message for me here" rows="4"/>
+                                <Form.Control as="textarea" rows={3} type="message" placeholder="Enter your message for me here" rows="4" />
                             </Form.Group>
-                            <Button className="btn btn-dark textUppercase" variant="primary" type="submit">
+                            <Button className="btn btn-dark textUppercase" variant="primary" type="submit" onClick={handleFormSubmit}>
                                 Submit
                             </Button>
                         </Form>
+                        {errorMessage && (
+                            <div>
+                                <p className="error-text">{errorMessage}</p>
+                            </div>
+                        )}
 
                     </div>
                     <div className="row">
